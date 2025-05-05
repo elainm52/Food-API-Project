be0c72fcff8c23d7c128fbbc067bdc6dad6b7fea
Elain Polakova <S00250500@atu.ie>
2025-05-05 15:37:22 +0100

    put parameters into the interface file and wrote some code in the services ts file

--- food-api-project/src/app/interfaces/food-api-response.ts
+++ food-api-project/src/app/interfaces/food-api-response.ts
@@ -1,2 +1,52 @@
 export interface FoodApiResponse {
+  results: Recipe[];
+  offset: number;
+  number: number;
+  totalResults: number;
 }
+export interface Recipe {
+    id: number;
+    title: string;
+    image: string;
+    imageType: string;
+    readyInMinutes: number; 
+    servings: number; 
+    calories: number; 
+    cuisines: string[]; 
+    dishTypes: string[]; 
+    diets: string[]; 
+    glutenFree: boolean;
+    dairyFree: boolean;
+    vegetarian: boolean;
+    vegan: boolean;
+    ingredients: Ingredient[]; 
+    equipment: Equipment[]; 
+    nutrition: Nutrition; 
+  }
+  
+  export interface Ingredient {
+    id: number;
+    name: string;
+    amount: number;
+    unit: string; 
+  }
+  
+  export interface Equipment {
+    id: number;
+    name: string;
+    image: string; 
+  }
+  
+  export interface Nutrition {
+    protein: string; 
+    carbs: string; 
+    fat: string; 
+    calories: string; 
+    nutrients: Nutrient[]; 
+  }
+  
+  export interface Nutrient {
+    name: string; 
+    amount: number; 
+    unit: string; 
+  }
--- food-api-project/src/app/services/food-api.service.ts
+++ food-api-project/src/app/services/food-api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable,throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-
+import { HttpParams } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { FoodApiResponse, Recipe } from '../interfaces/food-api-response';
 
 
 @Injectable({
@@ -10,5 +11,66 @@ import { catchError, tap } from 'rxjs/operators';
 })
 export class FoodApiService {
 
-  constructor() { }
+  private apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+  private apiKey = 'b2d8bb37dede437f90ddb167ac0e3bf4';
+  
+  
+  constructor(private http: HttpClient) {}
+
+   getRecipes(
+    ingredients: string[],
+    maxReadyTime?: number,
+    intolerances?: string[],
+    diet?: string
+  ): Observable<FoodApiResponse> {
+    let params = new HttpParams()
+      .set('apiKey', this.apiKey)
+      .set('addRecipeInformation', 'true')
+      .set('addRecipeNutrition', 'true')
+      .set('number', '10'); 
+
+    // Add optional parameters
+    if (ingredients.length > 0) {
+      params = params.set('includeIngredients', ingredients.join(','));
+    }
+    if (maxReadyTime) {
+      params = params.set('maxReadyTime', maxReadyTime.toString());
+    }
+    if (intolerances && intolerances.length > 0) {
+      params = params.set('intolerances', intolerances.join(','));
+    }
+    if (diet) {
+      params = params.set('diet', diet);
+    }
+
+      // Make the HTTP GET request
+      return this.http.get<FoodApiResponse>(this.apiUrl, { params }).pipe(
+        map((response: FoodApiResponse) => response), // Map the response to match the interface
+        catchError(this.handleError) // Handle errors
+      );
+    }
+
+    getRecipeById(recipeId: number): Observable<Recipe> {
+      const url = `https://api.spoonacular.com/recipes/${recipeId}/information`;
+      const params = new HttpParams().set('apiKey', this.apiKey);
+  
+      return this.http.get<Recipe>(url, { params }).pipe(
+        map((response: Recipe) => response), // Map the response to match the Recipe interface
+        catchError(this.handleError) // Handle errors
+      );
+    }
+
+    private handleError(error: HttpErrorResponse): Observable<never> {
+      let errorMessage = 'An unknown error occurred!';
+      if (error.error instanceof ErrorEvent) {
+        // Client-side error
+        errorMessage = `Error: ${error.error.message}`;
+      } else {
+        // Server-side error
+        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      }
+      console.error(errorMessage);
+      return throwError(() => new Error(errorMessage));
+    }
+
 }
