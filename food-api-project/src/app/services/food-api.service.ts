a96ad7097ced37197ef77c75f20968dc2ce27f49
Elain Polakova <S00250500@atu.ie>
2025-05-05 14:29:28 +0100

    interface file created

--- food-api-project/src/app/services/food-api.service.ts
+++ food-api-project/src/app/services/food-api.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable,throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
