ddcf817a824d7fffa07da71223b6bcf26e5097a3
Elain Polakova <S00250500@atu.ie>
2025-05-09 00:15:00 +0100

    fixed the double search bar issue. Had to remove code in app.routes.ts file

--- food-api-project/src/app/app.routes.ts
+++ food-api-project/src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { SearchComponent } from './components/search/search.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 
 export const routes: Routes = [ // Add export here
-  { path: '', component: SearchComponent },
   { path: 'recipe/:id', component: RecipeDetailsComponent }
 ];
